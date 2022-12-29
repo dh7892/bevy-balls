13ca463b4a7775c6c414d7e725ca4ceb93a39572
dh7892 <dave@dhills.net>
2022-12-29 10:11:59 +0000

    hover working (kinda)

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::helpers::hex_grid::neighbors::{HexDirection, HexNeighbors};
 use bevy_ecs_tilemap::prelude::*;
 mod helpers;
-use helpers::camera::movement as camera_movement;
 use bevy_inspector_egui::WorldInspectorPlugin;
+use helpers::camera::movement as camera_movement;
 
 // Press SPACE to change map type. Hover over a tile to highlight its label (red) and those of its
 // neighbors (blue). Press and hold one of keys 0-5 to mark the neighbor in that direction (green).
@@ -16,34 +16,34 @@ const MAP_SIDE_LENGTH_X: u32 = 4;
 const MAP_SIDE_LENGTH_Y: u32 = 4;
 
 const TILE_SIZE_HEX_ROW: TilemapTileSize = TilemapTileSize { x: 50.0, y: 58.0 };
-const _TILE_SIZE_HEX_COL: TilemapTileSize = TilemapTileSize { x: 58.0, y: 50.0 };
 const GRID_SIZE_HEX_ROW: TilemapGridSize = TilemapGridSize { x: 50.0, y: 58.0 };
-const _GRID_SIZE_HEX_COL: TilemapGridSize = TilemapGridSize { x: 58.0, y: 50.0 };
 
 #[derive(Deref, Resource)]
-pub struct TileHandleHexRow(Handle<Image>);
+pub struct TileHandleHex(Handle<Image>);
 
-#[derive(Deref, Resource)]
-pub struct TileHandleHexCol(Handle<Image>);
+impl FromWorld for TileHandleHex {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+        Self(asset_server.load("hex.png"))
+    }
+}
+
 #[derive(Deref, Resource)]
 pub struct FontHandle(Handle<Font>);
-
-impl FromWorld for TileHandleHexCol {
-    fn from_world(world: &mut World) -> Self {
-        let asset_server = world.resource::<AssetServer>();
-        Self(asset_server.load("bw-tile-hex-col.png"))
-    }
-}
-impl FromWorld for TileHandleHexRow {
-    fn from_world(world: &mut World) -> Self {
-        let asset_server = world.resource::<AssetServer>();
-        Self(asset_server.load("bw-tile-hex-row.png"))
-    }
-}
 impl FromWorld for FontHandle {
     fn from_world(world: &mut World) -> Self {
         let asset_server = world.resource::<AssetServer>();
         Self(asset_server.load("fonts/FiraSans-Bold.ttf"))
+    }
+}
+
+#[derive(Deref, Resource)]
+pub struct TileHandleHexHover(Handle<Image>);
+
+impl FromWorld for TileHandleHexHover {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+        Self(asset_server.load("hex-hovered.png"))
     }
 }
 
@@ -57,22 +57,27 @@ impl FromWorld for AxeHandle {
 }
 
 // Add a single axe icon over the top of the tile at 0,0
-fn add_wood_cutter(mut commands: Commands,
+fn add_wood_cutter(
+    mut commands: Commands,
     _tilemap_q: Query<(&Transform, &TilemapType, &TilemapGridSize, &TileStorage)>,
     _tile_q: Query<&mut TilePos>,
     axe_handle: Res<AxeHandle>,
 ) {
     let scale = Vec3::new(0.7, 0.7, 1.0);
-    let translation =Vec3::new(0.0, 0.0, 1.1); 
-    commands.spawn((SpriteBundle{
+    let translation = Vec3::new(0.0, 0.0, 1.1);
+    commands.spawn(SpriteBundle {
         texture: axe_handle.clone(),
-        transform: Transform {translation: translation, scale: scale, ..Default::default() },
+        transform: Transform {
+            translation: translation,
+            scale: scale,
+            ..Default::default()
+        },
         ..Default::default()
-    }));
+    });
 }
 
 // Generates the initial tilemap, which is a hex grid.
-fn spawn_tilemap(mut commands: Commands, tile_handle_hex_row: Res<TileHandleHexRow>) {
+fn spawn_tilemap(mut commands: Commands, tile_handle_hex: Res<TileHandleHex>) {
     commands.spawn(Camera2dBundle::default());
 
     let map_size = TilemapSize {
@@ -100,7 +105,7 @@ fn spawn_tilemap(mut commands: Commands, tile_handle_hex_row: Res<TileHandleHexR
         grid_size,
         size: map_size,
         storage: tile_storage,
-        texture: TilemapTexture::Single(tile_handle_hex_row.clone()),
+        texture: TilemapTexture::Single(tile_handle_hex.clone()),
         tile_size,
         map_type,
         transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
@@ -151,6 +156,25 @@ fn spawn_tile_labels(
 #[derive(Component)]
 struct Hovered;
 
+#[derive(Component)]
+struct HoverCursor {
+    tile_pos: Option<TilePos>,
+}
+
+// Add a the hover cursor (currently not on a tile)
+fn add_hover_cursor(mut commands: Commands, hex_tile_hover: Res<TileHandleHexHover>) {
+    commands.spawn((
+        HoverCursor { tile_pos: None },
+        SpriteBundle {
+            texture: hex_tile_hover.clone(),
+            visibility: Visibility { is_visible: false },
+            transform: Transform {
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+    ));
+}
 // Converts the cursor position into a world position, taking into account any transforms applied
 // the camera.
 pub fn cursor_pos_in_world(
@@ -203,7 +227,62 @@ pub fn update_cursor_pos(
 }
 
 // This is where we check which tile the cursor is hovered over.
-fn hover_highlight_tile_label(
+// We need:
+// timemap
+// cursor_pos so we know what the mouse is pointing to
+// current highlighted tile
+fn update_hover_cursor(
+    mut commands: Commands,
+    cursor_pos: Res<CursorPos>,
+    tilemap_q: Query<
+        (
+            &TilemapSize,
+            &TilemapGridSize,
+            &TilemapType,
+            &TileStorage,
+            &Transform,
+        ),
+        (Without<HoverCursor>,),
+    >,
+    mut hover_cursor_q: Query<(Entity, &mut HoverCursor, &mut Visibility, &mut Transform)>,
+    tile_pos_q: Query<&TilePos>,
+) {
+    for (map_size, grid_size, map_type, tile_storage, map_transform) in tilemap_q.iter() {
+        // Grab the cursor position from the `Res<CursorPos>`
+        let cursor_pos: Vec3 = cursor_pos.0;
+        // We need to make sure that the cursor's world position is correct relative to the map
+        // due to any map transformation.
+        let cursor_in_map_pos: Vec2 = {
+            // Extend the cursor_pos vec3 by 1.0
+            let cursor_pos = Vec4::from((cursor_pos, 1.0));
+            let cursor_in_map_pos = map_transform.compute_matrix().inverse() * cursor_pos;
+            cursor_in_map_pos.xy()
+        };
+        // Once we have a world position we can transform it into a possible tile position.
+        if let Some(tile_pos) =
+            TilePos::from_world_pos(&cursor_in_map_pos, map_size, grid_size, map_type)
+        {
+            if let Some(tile_entity) = tile_storage.get(&tile_pos) {
+                for (_, hc, mut vis, mut trans) in hover_cursor_q.iter_mut() {
+                    // The mouse cursor is over a tile, so we need to set the hover_cursor
+                    // position and make the sprite visible
+                    let tile_pos = tile_pos_q.get(tile_entity).unwrap();
+                    let tile_center = tile_pos.center_in_world(grid_size, map_type).extend(1.1);
+                    let hover_location = *map_transform * Transform::from_translation(tile_center);
+                    trans.translation = hover_location.translation;
+                    vis.is_visible = true;
+                }
+            }
+        } else {
+            // The cursor has moved off the tiles so hide the cursor
+            // for (_, hc, mut vis, mut trans) in hover_cursor_q.iter_mut() {
+            //     vis.is_visible = false;
+            // }
+        }
+    }
+}
+
+fn hover_highlight(
     mut commands: Commands,
     cursor_pos: Res<CursorPos>,
     tilemap_q: Query<(
@@ -213,22 +292,9 @@ fn hover_highlight_tile_label(
         &TileStorage,
         &Transform,
     )>,
-    highlighted_tiles_q: Query<Entity, With<Hovered>>,
-    tile_label_q: Query<&TileLabel>,
-    mut text_q: Query<&mut Text>,
+    hovered_tiles_q: Query<&TilePos, With<Hovered>>,
+    hex_tile_hover: Res<TileHandleHexHover>,
 ) {
-    // Un-highlight any previously highlighted tile labels.
-    for highlighted_tile_entity in highlighted_tiles_q.iter() {
-        if let Ok(label) = tile_label_q.get(highlighted_tile_entity) {
-            if let Ok(mut tile_text) = text_q.get_mut(label.0) {
-                for mut section in tile_text.sections.iter_mut() {
-                    section.style.color = Color::BLACK;
-                }
-                commands.entity(highlighted_tile_entity).remove::<Hovered>();
-            }
-        }
-    }
-
     for (map_size, grid_size, map_type, tile_storage, map_transform) in tilemap_q.iter() {
         // Grab the cursor position from the `Res<CursorPos>`
         let cursor_pos: Vec3 = cursor_pos.0;
@@ -245,16 +311,21 @@ fn hover_highlight_tile_label(
             TilePos::from_world_pos(&cursor_in_map_pos, map_size, grid_size, map_type)
         {
             // Highlight the relevant tile's label
-            if let Some(tile_entity) = tile_storage.get(&tile_pos) {
-                if let Ok(label) = tile_label_q.get(tile_entity) {
-                    if let Ok(mut tile_text) = text_q.get_mut(label.0) {
-                        for mut section in tile_text.sections.iter_mut() {
-                            section.style.color = Color::RED;
-                        }
-                        commands.entity(tile_entity).insert(Hovered);
-                    }
-                }
-            }
+            // if let Some(tile_entity) = tile_storage.get(&tile_pos) {
+            //     let tile_pos = tile_q.get(tile_entity).unwrap();
+            //     let tile_center = tile_pos.center_in_world(grid_size, map_type).extend(1.1);
+            //     let hover_location = *map_transform * Transform::from_translation(tile_center);
+
+            //     commands.entity(tile_entity).insert(Hovered);
+
+            //     let hover_cursor = commands.spawn(SpriteBundle{
+            //         texture: hex_tile_hover.clone(),
+            //         transform: hover_location,
+            //         ..Default::default()
+            //     }).id();
+            //     commands.entity(tile_entity).push_children(&[hover_cursor]);
+
+            // }
         }
     }
 }
@@ -375,16 +446,17 @@ fn main() {
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(TilemapPlugin)
         .init_resource::<CursorPos>()
-        .init_resource::<TileHandleHexCol>()
-        .init_resource::<TileHandleHexRow>()
+        .init_resource::<TileHandleHexHover>()
+        .init_resource::<TileHandleHex>()
         .init_resource::<FontHandle>()
         .init_resource::<AxeHandle>()
         .add_startup_system(spawn_tilemap)
         // .add_startup_system_to_stage(StartupStage::PostStartup, spawn_tile_labels)
         .add_startup_system_to_stage(StartupStage::PostStartup, add_wood_cutter)
+        .add_startup_system_to_stage(StartupStage::PostStartup, add_hover_cursor)
         .add_system_to_stage(CoreStage::First, camera_movement)
         .add_system_to_stage(CoreStage::First, update_cursor_pos.after(camera_movement))
-        // .add_system(hover_highlight_tile_label)
-        // .add_system(highlight_neighbor_label.after(hover_highlight_tile_label))
+        .add_system(hover_highlight)
+        .add_system(update_hover_cursor)
         .run();
 }
